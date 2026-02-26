2bb9b2bedf5bec426a286ae11eb49fa622031e02
schroneko <schroneko@users.noreply.github.com>
2026-02-26 17:24:50 +0900

    Improve mobile responsiveness

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ app.get("/", (c) => {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <title>Lean Canvas</title>
         <meta
           name="description"
